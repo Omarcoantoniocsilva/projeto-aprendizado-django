3b638efdf5e323e9b71470a7683291981af24898
Marco Antônio Cardoso da Silva <marcoaantoniocsilva@gmail.com>
2025-08-29 07:53:32 -0300

    More about HTTP Protocol

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
+
+
 def my_view(request):
     return HttpResponse('UMA LINDA STRING')
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
+    path('outra-coisa-qualquer', my_view),
 ]
