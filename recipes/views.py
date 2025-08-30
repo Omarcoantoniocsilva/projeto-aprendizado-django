81ecb30eb2d9bbdeac26222682f980ae15fa7add
Marco Antônio Cardoso da Silva <marcoaantoniocsilva@gmail.com>
2025-08-29 21:57:53 -0300

    Mooving code to recipes app

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('HOME 2')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+def contato(request):
+    return HttpResponse('CONTATO')
